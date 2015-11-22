8c2898251994c5c1be13ca792712b860cf8251dd
Andres Rios <rianby64@gmail.com>
2015-11-22 06:56:40 +0300

    initial release (0.0.4) version

---
+++ test/demo.test.js
@@ -0,0 +1,36 @@
+'use strict';
+feature("Feature1", function() {
+
+  scenario("Scenario1", function(str3, str4) {
+    when('step3', function() {
+      console.log("feature1 scenario1 step3");
+    });
+
+    then('step4', function() {
+      console.log("feature1 scenario1 step4");
+    });
+  });
+
+  given('step2', function() {
+    console.log("feature1 step2");
+  });
+});
+
+scenario('Scenario2', function() {
+  given('step1', function() {
+    console.log('scenario2 step1');
+  })
+  given('step2', function() {
+    console.log("scenario2 step2");
+  });
+  when('step3', function() {
+    console.log("scenario2 step3");
+  });
+  then('step4', function() {
+    console.log("scenario2 step4");
+  });
+});
+
+given('step1', function() {
+  console.log("step1");
+});
---
+++ test/feature/demo.feature
@@ -0,0 +1,13 @@
+Feature: Feature1
+
+  Scenario: Scenario1
+    Given step1
+    And step2
+    When step3
+    Then step4
+
+  Scenario: Scenario2
+    Given step1
+    And step2
+    When step3
+    Then step4
